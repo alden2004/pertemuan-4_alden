77e040117ede983895130efca4b0c736fb2a8734
alden2004 <152677333+alden2004@users.noreply.github.com>
2024-11-09 20:58:59 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+    return (
+      <div className="text-2xl flex justify-center bg-blue-500 min-h-screen items-center">
+        <div>
+          <button
+            className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+            type="submit"
+          >
+            Belanja Sekarang
+          </button>
+        </div>
+      </div>
+    );
+  }
